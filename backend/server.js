696834c3d7cb105f492e2363f9fdff31d80f59fc
Sunil Hirole <sunilhirole@gmail.com>
2023-02-03 18:07:09 +0530

    Added backend controllers for signup and login

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { chats } = require('./data/data')
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
-dotenv.config()
+dotenv.config();
+connectDB();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/chat', (req, res) => {
- res.send(chats);
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat/:id', (req, res) => {
-  const singleChat = chats.find((chat) =>
-    chat.id === req.params.id)
- res.send(singleChat);
-})
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat.id === req.params.id);
+  res.send(singleChat);
+});
 
-const PORT = process.env.PORT || 5000
+app.use("/api/user", userRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
